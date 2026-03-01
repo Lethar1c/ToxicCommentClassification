872cc54de7f19e2797970ed06211a2f98fc30f72
Lethar1c <letharicimport@gmail.com>
2026-03-01 22:36:32 +0300

    debug

--- sandbox.py
+++ sandbox.py
@@ -10,7 +10,7 @@ train_loader, val_loader, test_loader = get_tfidf_data_loaders()
 device = "cuda" if torch.cuda.is_available() else 'cpu'
 print("Running on " + device)
 
-for x, y in train_loader():
+for x, y in train_loader:
     print(x)
     print(y)
     print(MLP_model(x))
