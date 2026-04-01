8e42c8e151965036197b83709842daa2ccb6a5af
Lethar1c <letharicimport@gmail.com>
2026-04-01 20:35:35 +0300

    feat: update trainer for logistic regression

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -14,7 +14,10 @@ def get_metrics(model, val_loader, test_loader, device, prob_count=200):
     :param alpha: - порог для весов
     :return: ``(accuracy,recall,precision,F1)``
     '''
-    model.eval()
+    try:
+        model.eval()
+    except:
+        pass
     tp = 0
     tn = 0
     fp = 0
@@ -57,12 +60,58 @@ y_pred = {y_pred}
 Current batch info:
 x_mean - {x.mean()}
 x_std - {x.std()}""")
-                for i, param in enumerate(model.parameters()):
-                    if isinstance(param, torch.Tensor):
-                        print(f"Param {i} -- mean = {param.mean()}, std = {param.std()}, max = {param.max()}, min = {param.min()}")
+                # for i, param in enumerate(model.parameters()):
+                #     if isinstance(param, torch.Tensor):
+                #         print(f"Param {i} -- mean = {param.mean()}, std = {param.std()}, max = {param.max()}, min = {param.min()}")
 
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
     accuracy = (tp + tn) / (fp + fn + tp + tn)
     f1 = 2 * precision * recall / (precision + recall)
+    return accuracy, recall, precision, f1, max_prob
+
+def sigmoid(z):
+    return 1/(1 + np.exp(-z))
+
+def get_regression_metrics(regression, x_val, y_val, x_test, y_test, prob_count=200):
+    '''
+    Get model metrics (accuracy, recall, precision, F1)
+    :param model:
+    :param val_loader:
+    :param test_loader:
+    :param alpha: - порог для весов
+    :return: ``(accuracy,recall,precision,F1)``
+    '''
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    max_prob = 0
+    probs = np.linspace(1e-4, 1-1e-4, prob_count)
+    y_pred = regression.predict_proba(x_val)
+    max_f1 = -1
+    for prob in tqdm(probs):
+        y_pred_01_val = (y_pred > prob).astype(int)
+        val_tp = ((y_val == 1) & (y_pred_01_val == 1)).sum()
+        val_tn = ((y_val == 0) & (y_pred_01_val == 0)).sum()
+        val_fp = ((y_val == 0) & (y_pred_01_val == 1)).sum()
+        val_fn = ((y_val == 1) & (y_pred_01_val == 0)).sum()
+        val_precision = val_tp / (val_tp + val_fp + 1e-9)
+        val_recall = val_tp / (val_tp + val_fn + 1e-9)
+        val_f1 = 2 * val_precision * val_recall / (val_precision + val_recall + 1e-9)
+        if val_f1 > max_f1:
+            max_f1 = val_f1
+            max_prob = prob
+
+    y_pred = regression.predict_proba(x_test)
+    y_pred_01 = (y_pred > max_prob).astype(int)
+    tp += ((y_test == 1) & (y_pred_01 == 1)).sum()
+    tn += ((y_test == 0) & (y_pred_01 == 0)).sum()
+    fp += ((y_test == 0) & (y_pred_01 == 1)).sum()
+    fn += ((y_test == 1) & (y_pred_01 == 0)).sum()
+
+    precision = tp / (tp + fp + 1e-9)
+    recall = tp / (tp + fn + 1e-9)
+    accuracy = (tp + tn) / (fp + fn + tp + tn + 1e-9)
+    f1 = 2 * precision * recall / (precision + recall + 1e-9)
     return accuracy, recall, precision, f1, max_prob
